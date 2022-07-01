12d84be33c58e622aab24bc659ed769e2d15362d
ajTronic <83540738+ajTronic@users.noreply.github.com>
2022-07-01 14:16:59 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Timer from "./Components/Timer.js"
+
+function App() {
+  return (
+    <Timer/>
+  );
+}
+
+export default App;
---
+++ src/Components/Timer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Icon } from '@iconify/react';
+import Button from '@mui/material/Button';
+import PlayArrowSharpIcon from '@mui/icons-material/PlayArrowSharp';
+import TimerManager from "easytimer.js";
+import PauseSharpIcon from '@mui/icons-material/PauseSharp';
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.props = props
+        this.timer = new TimerManager()
+        this.state = {
+            timeLeft: "10m 07s",
+            timeElapesd: this.timer.getTimeValues().toString(),
+            on: false
+        }
+    }
+
+    componentDidMount() {
+        this.timer.addEventListener('secondsUpdated', () => {
+            this.setState({ timeElapesd: this.timer.getTimeValues().toString() })
+        })
+    }
+
+    componentDidUpdate() {
+        if (this.state.on) this.timer.start()
+        else this.timer.pause()
+    }
+
+    render = () => {
+        return (
+            <div className='Timer'>
+                <div className='Time'>{this.state.timeElapesd}</div>
+                <Button variant="outlined" className='Btn' onClick={
+                    () => this.setState({on: !this.state.on})
+                }>
+                    {this.state.on ? <PauseSharpIcon/> : <PlayArrowSharpIcon/>}
+                    {this.state.on ? "Stop" : "Start"}
+                </Button>
+            </div>
+        );
+    }
+}
+
+export default Timer
