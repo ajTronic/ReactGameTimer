3315396a17c2714263544788752fb60fb4323417
ajTronic <83540738+ajTronic@users.noreply.github.com>
2022-07-02 20:12:59 +0100

    Added a secondary timer

---
+++ src/Components/BaseTimer.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import PlayArrowSharpIcon from '@mui/icons-material/PlayArrowSharp';
+import TimerManager from "easytimer.js";
+import PauseSharpIcon from '@mui/icons-material/PauseSharp';
+
+class BaseTimer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.props = props
+        this.timer = new TimerManager()
+        this.timer.start({ countdown: true, startValues: this.props.settings });
+        this.timer.pause()
+        this.state = {
+            timeElapesd: this.timer.getTimeValues().toString(),
+            on: false,
+            btnState: "Start",
+            finished: false
+        }
+    }
+
+    componentDidMount() {
+        this.timer.addEventListener('secondsUpdated', () => {
+            this.setState({ timeElapesd: this.timer.getTimeValues().toString() })
+        })
+
+        this.timer.addEventListener('targetAchieved', () => {
+            this.finish()
+        })
+    }
+
+    finish() {
+        this.setState({ finished: true })
+        var audio = new Audio('./finished.ogg')
+        audio.play()
+        document.querySelector(".Time").classList.add("Finished")
+
+        this.timer.stop()
+        this.timer.start({ countdown: false })
+    }
+
+    render = () => {
+        return (
+            <div className='BaseTimer'>
+                <div className='Time'>
+                    {this.state.finished ? '-' : null}
+                    {this.state.timeElapesd}
+                </div>
+                <Button variant="outlined" className='Btn' onClick={
+                    () => this.setState({ on: !this.state.on }, () => {
+                        if (this.state.on) {
+                            this.timer.start();
+                        } else {
+                            this.timer.pause()
+                        }
+                    })
+                }>
+                    {this.state.on ? <PauseSharpIcon /> : <PlayArrowSharpIcon />}
+                    {this.state.on ? "Stop" : "Start"}
+                </Button>
+            </div>
+        );
+    }
+}
+
+export default BaseTimer
---
+++ src/Components/SecondaryTimer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class SecondaryTimer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="SecondaryTimer">
+                SecondaryTimer
+            </div>
+        );
+    }
+}
+
+export default SecondaryTimer;
